0b69852bc635a696b9c41270799d76faf19bd8c1
Ginko-san <21salchi21@gmail.com>
2017-11-09 13:48:13 -0600

    Level Generation

--- Assets/js/gameScript.js
+++ Assets/js/gameScript.js
@@ -7,20 +7,68 @@ var APPLICATION = APPLICATION || {}; // check if the NameSpace exist as var or f
 
 
 // Defining Objects
+// 27 * 22
+function Level(w, pO, c, r, s) {
+    this.walls = w;
+    this.primaryObjects = pO;
+    this.objectsArrayFill = [];
+    this.columns = c;
+    this.rows = r;
+    this.spriteSize = s;
+
+}
+
+// Fill the array with empty values
+//  0 = empty
+Level.prototype.initObjectArray = function () {
+    var cont = 0;
+    for (var i=0; i<this.columns; i++) {
+        for (var j=0; j<this.rows; j++) {
+            this.objectsArrayFill[cont] = { type: 0, dx: i, dy: j };
+            cont++;
+        }
+    }
+};
+
+// Fill the array with walls
+//  0 = empty
+//  1 = primaryObject
+//  2 = wall
+Level.prototype.placeObjects = function ( typeObject) {
+    var nObjects = (typeObject === 1) ? this.primaryObjects : this.walls;
+
+    for (var i=0; i < nObjects; i++) {
+        while (true) {
+            var x = Math.floor((Math.random() * (this.columns-2)) +1);
+            var y = Math.floor((Math.random() * (this.rows-2)) +1);
+
+            var place = (y * this.columns) + x;
+
+            console.log("Place: " + place + ", dx: " + x + ", dy: " + y + ";");
+            if (this.objectsArrayFill[place].type === 0) {
+                this.objectsArrayFill[place].type = (typeObject === 1) ? 1 : 2;
+                this.objectsArrayFill[place].dx = x;
+                this.objectsArrayFill[place].dy = y;
+                break;
+            }
+        }
+    }
+};
+
+
+
+
 
 function playerTank() {
     this.image = new Image();
     this.image.src = '../images/tank_player.png';
     this.image.onload = function(){
         context.drawImage(base_image, 100, 100);
-    }
+    };
     
     this.size = 32;
 
 }
-
-
-
 
 
 var canvas = document.getElementById("canvasTankGame");
@@ -46,29 +94,34 @@ var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
+var wallImage = document.getElementById('wall');
+var pOImage = document.getElementById('primaryObject');
+
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
 function keyDownHandler(e) {
-    if(e.keyCode == 39) {
+    if(e.keyCode === 39) {
         rightPressed = true;
     }
-    else if(e.keyCode == 37) {
+    else if(e.keyCode === 37) {
         leftPressed = true;
     }
 }
 function keyUpHandler(e) {
-    if(e.keyCode == 39) {
+    if(e.keyCode === 39) {
         rightPressed = false;
     }
-    else if(e.keyCode == 37) {
+    else if(e.keyCode === 37) {
         leftPressed = false;
     }
 }
 
-function draw() {
+function draw(level) {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // This line clear the canvas every 10 miliseconds
 
+    drawObjects(level);
     drawBall(ballColor);
     drawPaddle();
 
@@ -93,7 +146,7 @@ function draw() {
     if(rightPressed && paddleX < canvas.width-(wallSize+paddleWidth)) {
         paddleX += 7;
     }
-    else if(leftPressed && paddleX > 0+wallSize) {
+    else if(leftPressed && paddleX > wallSize) {
         paddleX -= 7;
     }
 
@@ -129,4 +182,23 @@ function getRandomColor() {
     return color;
 }
 
-setInterval(draw, 10);
+function drawObjects(level) {
+    var cont = level.columns * level.rows;
+
+    for (var i=0; i<cont; i++) {
+            if (level.objectsArrayFill[i].type === 0) { continue; }
+            ctx.beginPath();
+            ctx.drawImage((level.objectsArrayFill[i].type === 1) ? pOImage : wallImage,
+                           level.objectsArrayFill[i].dx*32,
+                           level.objectsArrayFill[i].dy*32);
+            ctx.fill();
+            ctx.closePath();
+    }
+}
+
+var level1 = new Level(100, 3, 27, 22, 32);
+level1.initObjectArray();
+level1.placeObjects(1); // put all the walls in place
+level1.placeObjects(2); // put all the primaryObjects in place
+
+setInterval(draw(level1), 10);
